a455ab9ccac186901dde25b87ed7065b30768e47
an-Iceberg <a.g.marosi@bluewin.ch>
2022-11-23 12:53:15 +0100

    added string formatting example

--- stringManipulation/stringManipulation.go
+++ stringManipulation/stringManipulation.go
@@ -17,4 +17,10 @@ func Go() {
 	message = strings.ReplaceAll(message, "=", ":")
 
 	fmt.Println(message)
+
+	number := 74
+
+  // Formatting with positioned arguments
+	fmt.Printf("%%v:%[1]v %%+v:%+[1]v %%#v:%#[1]v %%T:%[1]T\n", number)
+	fmt.Printf("%#[1]b 0d%[1]d %#[1]o %[1]O %#[1]x %#[1]X %[1]U\n", number)
 }
